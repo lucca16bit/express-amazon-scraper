f5d7564bb2059c343e2f01664afb133e92ab2ce8
lucas <lucaston2323@gmail.com>
2025-03-25 17:47:27 -0300

    styles: estilizar o conteúdo dos produtos

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,39 @@
 const searchBtn = document.querySelector('.search-btn');
-const keyword = document.querySelector('.search-input').value;
 
-console.log(keyword);
+searchBtn.addEventListener('click', async () => {
+    const keyword = document.getElementById('search-input').value;
+    const results = document.querySelector('.results');
 
+    if (!keyword) {
+        console.log('Please enter a keyword');
+    }
+
+    try {
+        const response = await fetch(`http://localhost:3000/api/scrape?keyword=${encodeURIComponent(keyword)}`);
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch data from the backend');
+        }
+
+        const products = await response.json();
+        results.innerHTML = '';
+
+        products.forEach(prod => {
+            const cards = document.createElement('div');
+            cards.classList.add('card');
+
+            cards.innerHTML = `
+                <img src="${prod.imageUrl}" alt="${prod.title}" />
+                <h2>${prod.title}</h2>
+                <div class="card-rating">
+                    <p id="rating">${prod.rating}</p>
+                    <p id="reviews">${prod.reviews}</p>
+                </div>
+            `;
+            results.appendChild(cards);
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+});
